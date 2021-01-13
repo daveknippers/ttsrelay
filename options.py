10b2e8e0c1700727e1c1688dc1eac98167a097ba
Dave Knippers <dave.knippers@gmail.com>
2021-01-13 13:36:53 -0700

    fixed some formatting in options

--- options.py
+++ options.py
@@ -22,33 +22,33 @@ from collections import defaultdict
 # https://api.tabletopsimulator.com/player-color/
 player_colour_mapping = defaultdict(lambda: 'Grey', 
 	dict([('Hecuba','Teal'),
-			('Arkin','Red'),
-			('Meltor','White'),
-			('Cid','Blue'),
-			('Grezlik','Yellow'),
-			('Lannister','Brown'),
-			('Gorgulash','Green')]))
+		('Arkin','Red'),
+		('Meltor','White'),
+		('Cid','Blue'),
+		('Grezlik','Yellow'),
+		('Lannister','Brown'),
+		('Gorgulash','Green')]))
 
 # if any part of the key (the first part of the pair)
 # is in a message parsed from discord, it will be mapped
 # to the value (the second part of the pair)
 replace_map = {':game_die:':'Dice:',
-				':arrow_right:':'->',
-				'**':'',
-				'_':'',
-				':redcircle:':'',
-				':greencircle:':'',
-				':zero:':'0',
-				':one:':'1',
-				':two:':'2',
-				':three:':'3',
-				':four:':'4',
-				':five:':'5',
-				':six:':'6',
-				':seven:':'7',
-				':eight:':'8',
-				':nine:':'9',
-				':keycapten:':'10'}
+		':arrow_right:':'->',
+		'**':'',
+		'_':'',
+		':redcircle:':'',
+		':greencircle:':'',
+		':zero:':'0',
+		':one:':'1',
+		':two:':'2',
+		':three:':'3',
+		':four:':'4',
+		':five:':'5',
+		':six:':'6',
+		':seven:':'7',
+		':eight:':'8',
+		':nine:':'9',
+		':keycapten:':'10'}
 
 # Throw out any text line that starts with '||'
 disregard_details = True
